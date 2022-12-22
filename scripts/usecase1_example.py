85c4255eaf403216651f1f4e7a91da3a43de2b50
SarahAlidoost <f.alidoost@esciencecenter.nl>
2022-12-22 15:50:21 +0100

    run usecase1_example script for train and test

--- scripts/usecase1_example.py
+++ scripts/usecase1_example.py
@@ -1,3 +1,12 @@
+"""
+Train a (ML) model with day of first bloom from pheno observations as target and
+weather data as predictors. The observations are available for a few locations,
+whereas the weather data is available on a full raster. So, we want to apply the
+model for the full raster.
+
+output: 69.767
+"""
+
 from springtime import build_workflow, run_workflow
 
 # dataset and phenophases to test
@@ -15,22 +24,25 @@ ppo = {
 
 # daymet specifications
 daymet = {
-    "longitudes" : [-80, -79.5],
-    "latitudes" : [35, 35.1],
-    "var_names" : ['tmax', 'tmin', 'prcp'],
-    "years" : [2019, 2020],
+    "longitudes": [-80, -79.5],
+    "latitudes": [35, 35.1],
+    "var_names": ['tmax', 'tmin', 'prcp'],
+    "years": [2019, 2020],
+    "statistics": "annual monlthly average",
 }
 
 # create options
 options = {
     "obs_options": ppo,
     "eo_options": daymet,
-    "model_name": ["LinearRegression"],
+    "model_name": "LinearRegression",
     "train_test_strategy": "ShuffleSplit",
     "metric_name": "RMSE",
     "usecase_id": "1",
 }
 
 workflow_name = f"usecase1_{dataset}"
-# TODO not working yet
+
 workflow = build_workflow(options, name=workflow_name)
+results = run_workflow(workflow)
+print(results["metric_value"])

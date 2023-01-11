91eeea18f56437b5e4013c1398790120abac2069
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-01-11 17:10:28 +0100

    add raster prediction to the example script

--- scripts/usecase1_example.py
+++ scripts/usecase1_example.py
@@ -23,9 +23,9 @@ ppo_options = {
 daymet_options = {
     "longitudes": [-80, -79.5], # very high resolution data, so small area
     "latitudes": [35, 35.1],
-    "var_names": ['tmax', 'tmin', 'prcp'],
+    "var_names": ['tmax', 'tmin', 'prcp'], # features
     "years": [2019, 2020],
-    "statistics": "annual monlthly average",
+    "statistics": "annual monlthly average", # convert daily to monthly
 }
 
 # create options for workflow
@@ -38,6 +38,36 @@ options = {
     "usecase_id": "1",
 }
 
+# run workflow: prepare data, train model
 workflow = build_workflow(options, name="usecase1_daymet_ppo")
 results = run_workflow(workflow)
 print(results["metric_value"])
+
+# prediction
+## select data
+predictors = results["data"]["data_arrays"]["predictors"]
+
+## select one year, calculate annual average
+x_predict = predictors.sel(year=2019).mean("season").to_dataframe()[['tmax', 'tmin', 'prcp']]
+x_predict = x_predict.to_numpy()
+
+## predict
+model = results["model"]
+y_predict = model.predict(x_predict)
+
+## convert back to array
+import xarray as xr
+predicted_doy = y_predict.reshape(predictors.sizes['x'], predictors.sizes['y'], 1)
+
+da = xr.DataArray(
+    data=predicted_doy,
+    dims=["x", "y", "year"],
+    coords=dict(
+        x=("x", predictors.x.values),
+        y=("y", predictors.y.values),
+        year=[2019],
+    ),
+    attrs=dict(
+        description="Day of year.",
+    ),
+)

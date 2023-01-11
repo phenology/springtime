50df1a5c49238c2914212a005acd98e0b7e5b6b6
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-01-11 15:30:45 +0100

    clean example script, add some comments

--- scripts/usecase1_example.py
+++ scripts/usecase1_example.py
@@ -9,40 +9,35 @@ output: 69.767
 
 from springtime import build_workflow, run_workflow
 
-# dataset and phenophases to test
-dataset = "daymet_ppo"
-
 # ppo specifications
-ppo = {
+ppo_options = {
     "genus": "Syringa",
-    "source": "USA-NPN",
+    "source": "USA-NPN", # because daymet covers USA
     "year": "[2019 TO 2020]",
     "latitude": "[35 TO 38]",
     "longitude": "[-80 TO -75]",
-    "termID": "obo:PPO_0002313",
+    "termID": "obo:PPO_0002313", # true leaves present
 }
 
 # daymet specifications
-daymet = {
-    "longitudes": [-80, -79.5],
+daymet_options = {
+    "longitudes": [-80, -79.5], # very high resolution data, so small area
     "latitudes": [35, 35.1],
     "var_names": ['tmax', 'tmin', 'prcp'],
     "years": [2019, 2020],
     "statistics": "annual monlthly average",
 }
 
-# create options
+# create options for workflow
 options = {
-    "obs_options": ppo,
-    "eo_options": daymet,
+    "obs_options": ppo_options,
+    "eo_options": daymet_options,
     "model_name": "LinearRegression",
     "train_test_strategy": "ShuffleSplit",
     "metric_name": "RMSE",
     "usecase_id": "1",
 }
 
-workflow_name = f"usecase1_{dataset}"
-
-workflow = build_workflow(options, name=workflow_name)
+workflow = build_workflow(options, name="usecase1_daymet_ppo")
 results = run_workflow(workflow)
 print(results["metric_value"])

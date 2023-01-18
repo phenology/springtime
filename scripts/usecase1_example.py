50430d4c364af0b17bd88c7a98e507cdef0e3af1
SarahAlidoost <f.alidoost@esciencecenter.nl>
2023-01-18 18:54:09 +0100

    change data, remove plots from example script

--- scripts/usecase1_example.py
+++ scripts/usecase1_example.py
@@ -4,7 +4,7 @@ weather data as predictors. The observations are available for a few locations,
 whereas the weather data is available on a full raster. So, we want to apply the
 model for the full raster.
 
-output: 49.238
+output: 45.55
 """
 
 from springtime import build_workflow, run_workflow
@@ -14,15 +14,15 @@ ppo_options = {
     "genus": "Syringa",
     "source": "USA-NPN", # because daymet covers USA
     "year": "[2019 TO 2020]",
-    "latitude": "[35 TO 41]",
-    "longitude": "[-88 TO -84]",
+    "latitude": "[39.5 TO 40.1]",
+    "longitude": "[-86.5 TO -86]",
     "termID": "obo:PPO_0002313", # true leaves present
 }
 
 # daymet specifications
 daymet_options = {
-    "longitudes": [-88, -84], # very high resolution data, so small area
-    "latitudes": [35, 41],
+    "longitudes": [-86.5, -86], # very high resolution data, so small area
+    "latitudes": [39.5, 40.1],
     "var_names": ['tmax', 'tmin', 'prcp'], # features
     "years": [2019, 2020],
     "statistics": "annual seasonal average", # convert daily to seasonal
@@ -51,38 +51,15 @@ x_predict = x_predict.query("year == 2019")
 model = results["model"]
 y_predict = model.predict(x_predict)
 
-## visualization
-import matplotlib.pyplot as plt
-from cartopy import crs
-from springtime import data_utils
-
-### Field observations (truth) = PPO 
-obs = results["data"]["obs_data"]
-unique_idx = ['latitude', 'longitude', 'year']
-event_df = obs.groupby(unique_idx).dayOfYear.mean()
-event = event_df.reset_index()
-fig = plt.figure()
-ax = fig.add_subplot(projection=crs.PlateCarree())
-ax.coastlines()
-ax.set_xlim(-80, -78)
-ax.set_ylim(35, 37)
-sc = ax.scatter(event.longitude, event.latitude, c=event.dayOfYear)
-fig.savefig('observations', bbox_inches='tight')
-plt.colorbar(sc)
-
-### Raster Weather data
-# TODO add plotting
-eo = results["data"]["eo_data"]
-
-## Weather/ Satellite data (masked by observations)
-# TODO add plotting
-eo_obs = data_utils.merge_eo_obs(eo, obs)
-
-### Predicted truth
+### Predicted truth as xarray
 import xarray as xr
 predictors = results["data"]["eo_data"]
 predicted_doy = y_predict.reshape(predictors.sizes['x'], predictors.sizes['y'], 1)
 
+lat = predictors.lat.values[0,:,:]
+lat = lat.reshape(1, predictors.sizes['y'], predictors.sizes['x'])
+lon = predictors.lon.values[0,:,:]
+lon = lon.reshape(1, predictors.sizes['y'], predictors.sizes['x'])
 da = xr.DataArray(
     data=predicted_doy,
     dims=["x", "y", "year"],
@@ -90,9 +67,11 @@ da = xr.DataArray(
         x=("x", predictors.x.values),
         y=("y", predictors.y.values),
         year=[2019],
+        lat=(["year", "y", "x"], lat),
+        lon = (["year", "y", "x"], lon),
     ),
     attrs=dict(
         description="Day of year.",
     ),
 )
-da.plot()
+da.to_netcdf("DayOfYear_2019_predcited.nc")

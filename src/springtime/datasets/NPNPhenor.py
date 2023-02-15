6ed110780e61f720d449fd8996fb876d27750507
Peter Kalverla <peter.kalverla@gmx.com>
2023-02-15 18:32:38 +0100

    Add load function

--- src/springtime/datasets/NPNPhenor.py
+++ src/springtime/datasets/NPNPhenor.py
@@ -1,32 +1,33 @@
-import subprocess
-from typing import Iterable, Literal, Optional
+from typing import Literal, Optional
 
-import geopandas
+import geopandas as gpd
+import pandas as pd
 import rpy2.robjects as ro
 from pydantic import BaseModel
-from rpy2.robjects import pandas2ri, NULL
 from rpy2.robjects.packages import importr
 
 from springtime.config import CONFIG
 from springtime.datasets.daymet import NamedArea
 
 
-
 class NPNPhenor(BaseModel):
-    """Download and load data from https://pep725.eu .
+    """Download and load data from NPN.
 
     Uses phenor (https://bluegreen-labs.github.io/phenor/) as client.
+
+    Could use https://data.usanpn.org/observations/get-started to figure out
+    which species/phenophases combis are available.
 
     Example:
 
         ```python
         from springtime.datasets.NPNPhenor import NPNPhenor
-        dataset = NPNPhenor(species=36, phenophase=371, years=[2010, 2011])
+        dataset = NPNPhenor(species=36, phenophase=483, years=[2010, 2011])
         dataset.download()
-        df = dataset.load()  # TODO: implement
+        gdf = dataset.load()
 
-        # or with filters
-        dataset = NPNPhenor(species=36, phenophase=371, years=[2010, 2011], area={'name':'some', 'bbox':(4, 45, 8, 50)})
+        # or with area bounds
+        dataset = NPNPhenor(species=3, phenophase=371, years=[2010, 2011], area={'name':'some', 'bbox':(4, 45, 8, 50)})
         ```
 
     Requires phenor R package. Install with
@@ -34,17 +35,6 @@ class NPNPhenor(BaseModel):
     ```R
     devtools::install_github("bluegreen-labs/phenor@v1.3.1")
     ```
-
-    in R, these queries work:
-
-    ```R
-    library(phenor)
-    pr_dl_npn(species=3, phenophase=371, start="2010-01-01", end="2011-01-01", extent=NULL)  # red maple: breaking leaf buds
-    pr_dl_npn(species=36, phenophase=483, start="2010-01-01", end="2011-01-01", extent=NULL)  # common lilac: leaves
-    ```
-
-    Could use https://data.usanpn.org/observations/get-started to figure out
-    which species/phenophases combis are available.
 
     """
     dataset: Literal["NPNPhenor"] = "NPNPhenor"
@@ -61,10 +51,8 @@ class NPNPhenor(BaseModel):
     def _filename(self, year):
         """Path where files will be downloaded to and loaded from.
 
-        In phenor you can only specify dir, filename is chosen for you.
-
-        Otherwise, this would suffice:
-        # self.directory / f"{self.species}_{self.phenophase}_{year}.csv"
+        In phenor you can only specify dir, filename is chosen for you. So we
+        reproduce the filename that phenor creates.
         """
         phenor_filename = (
             f"phenor_npn_data_{self.species}_{self.phenophase}"
@@ -87,19 +75,23 @@ class NPNPhenor(BaseModel):
 
 
     def load(self):
-        """Load the dataset from disk into memory."""
-        raise NotImplementedError("To do")
+        """Load the dataset into memory."""
+        df = pd.concat([self._r_load(year) for year in range(*self.years)])
+        geometry = gpd.points_from_xy(df.pop('longitude'), df.pop('latitude'))
+        gdf = gpd.GeoDataFrame(df, geometry=geometry)
+        return gdf
 
     def _r_download(self, year):
         """Download data using phenor's download function.
 
         This executes R code in python using the rpy2 package.
         """
-        extent = NULL
+        phenor = importr("phenor")
+
+        extent = ro.NULL
         if self.area is not None:
             extent = self.area.bbox
 
-        phenor = importr("phenor")
         phenor.pr_dl_npn(
             species=self.species,
             phenophase=self.phenophase,
@@ -110,6 +102,13 @@ class NPNPhenor(BaseModel):
             path=str(self.directory),
         )
 
+    def _r_load(self, year) -> pd.DataFrame:
+        """Read data with r and return as (python) pandas dataframe."""
+        filename = str(self._filename(year))
+        readRDS = ro.r['readRDS']
+        data = readRDS(filename)
+        df = ro.pandas2ri.rpy2py_dataframe(data)
+        return df
 
-dataset = NPNPhenor(species=36, phenophase=483, years=[2010, 2011])
-dataset.download()
+
+# TODO: add methods to list species and phenophases

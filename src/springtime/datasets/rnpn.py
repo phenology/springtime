4f8bb08ea700e6cf56de57b091e3834e2209f420
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-05-02 11:37:12 +0200

    Make phenophase_ids required

--- src/springtime/datasets/rnpn.py
+++ src/springtime/datasets/rnpn.py
@@ -67,10 +67,11 @@ class RNPN(Dataset):
 
     dataset: Literal["RNPN"] = "RNPN"
     species_ids: Optional[NamedIdentifiers]
-    phenophase_ids: Optional[NamedIdentifiers]
+    phenophase_ids: NamedIdentifiers
     area: Optional[NamedArea] = None
     use_first: bool = True
     """When true uses first_yes columns as value, otherwise the last_yes columns."""
+    aggregation_operator: Literal["min", "max", "mean", "median"] = "min"
 
     def _filename(self, year):
         """Path where files will be downloaded to and loaded from.
@@ -103,29 +104,7 @@ class RNPN(Dataset):
         )
         geometry = gpd.points_from_xy(df.pop("longitude"), df.pop("latitude"))
         gdf = gpd.GeoDataFrame(df, geometry=geometry)
-        return gdf
-        if self.use_first:
-            gdf["datetime"] = pd.to_datetime(
-                {
-                    "year": gdf.first_yes_year,
-                    "month": gdf.first_yes_month,
-                    "day": gdf.first_yes_day,
-                }
-            )
-            var_name = self.phenophase_ids.name + "_doy"
-            gdf.rename(columns={"first_yes_doy": var_name}, inplace=True)
-            return gdf[["datetime", "geometry", var_name]]
-        else:
-            gdf["datetime"] = pd.to_datetime(
-                {
-                    "year": gdf.last_yes_year,
-                    "month": gdf.last_yes_month,
-                    "day": gdf.last_yes_day,
-                }
-            )
-            var_name = self.phenophase_ids.name + "_doy"
-            gdf.rename(columns={"last_yes_doy": var_name}, inplace=True)
-            return gdf[["datetime", "geometry", var_name]]
+        return _reformat(self, gdf)
 
     def download(self, timeout=30):
         """Download the data.
@@ -242,3 +221,32 @@ def npn_phenophase_ids_by_name(phenophase_name):
 def _lookup(df, column, expression):
     """Return rows where column matches expression."""
     return df[df[column].str.lower().str.contains(expression.lower())]
+
+
+def _reformat(self: RNPN, df):
+    var_name = self.phenophase_ids.name + "_doy"
+    if self.use_first:
+        df["datetime"] = pd.to_datetime(
+            {
+                "year": df.first_yes_year,
+                "month": df.first_yes_month,
+                "day": df.first_yes_day,
+            }
+        )
+        df.rename(columns={"first_yes_doy": var_name}, inplace=True)
+    else:
+        df["datetime"] = pd.to_datetime(
+            {
+                "year": df.last_yes_year,
+                "month": df.last_yes_month,
+                "day": df.last_yes_day,
+            }
+        )
+        df.rename(columns={"last_yes_doy": var_name}, inplace=True)
+
+    df = (
+        df[["datetime", "geometry", var_name]]
+        .groupby(by=[df.datetime.dt.year, "geometry"], as_index=False, sort=False)
+        .aggregate(self.aggregation_operator)
+    )
+    return gpd.GeoDataFrame(df)
---
+++ tests/datasets/test_rnpn.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+
+import geopandas as gpd
+from shapely import Point
+from pandas.testing import assert_frame_equal
+
+from springtime.datasets.rnpn import RNPN, _reformat
+
+
+def test_reformat_given_phenophase_ids_and_side_first_and_single_row():
+    dataset = RNPN(
+        phenophase_ids={"name": "Leaves", "items": [483]}, years=(2010, 2010)
+    )
+
+    df = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5)],
+            "phenophase_id": [483],
+            "first_yes_year": [2010],
+            "first_yes_month": [1],
+            "first_yes_day": [10],
+            "first_yes_doy": [10],
+        }
+    )
+
+    result = _reformat(dataset, df)
+
+    expected = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5)],
+            "datetime": [datetime(2010, 1, 10)],
+            "Leaves_doy": [10],
+        }
+    )
+    assert_frame_equal(result, expected)
+
+
+def test_reformat_given_phenophase_ids_and_side_first_and_double_row_per_year():
+    dataset = RNPN(
+        phenophase_ids={"name": "Leaves", "items": [483, 484]}, years=(2010, 2010)
+    )
+
+    df = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5), Point(4, 5), Point(4, 5)],
+            "phenophase_id": [483, 484, 483],
+            "first_yes_year": [2010, 2010, 2011],
+            "first_yes_month": [1, 1, 1],
+            "first_yes_day": [10, 20, 11],
+            "first_yes_doy": [10, 20, 11],
+        }
+    )
+
+    result = _reformat(dataset, df)
+
+    expected = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5), Point(4, 5)],
+            "datetime": [datetime(2010, 1, 10), datetime(2011, 1, 11)],
+            "Leaves_doy": [10, 11],
+        }
+    )
+    assert_frame_equal(result, expected)
+
+
+def test_reformat_given_phenophase_ids_and_side_last_and_single_row():
+    dataset = RNPN(
+        phenophase_ids={"name": "Leaves", "items": [483]},
+        years=(2010, 2010),
+        use_first=False,
+    )
+
+    df = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5)],
+            "phenophase_id": [483],
+            "last_yes_year": [2010],
+            "last_yes_month": [1],
+            "last_yes_day": [10],
+            "last_yes_doy": [10],
+        }
+    )
+
+    result = _reformat(dataset, df)
+
+    expected = gpd.GeoDataFrame(
+        {
+            "geometry": [Point(4, 5)],
+            "datetime": [datetime(2010, 1, 10)],
+            "Leaves_doy": [10],
+        }
+    )
+    assert_frame_equal(result, expected)

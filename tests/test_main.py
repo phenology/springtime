e524d70be67014633b51f2b8daf00fdc217c3eeb
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-05-03 11:50:37 +0200

    format

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,10 @@ import pytest
 
 from springtime.main import Workflow, cli, main
 
-@pytest.mark.skip(reason="PyPhenologyDataset does not return geometry and datetime columns yet")
+
+@pytest.mark.skip(
+    reason="PyPhenologyDataset does not return geometry and datetime columns yet"
+)
 def test_workflow():
     w = Workflow(
         datasets={
@@ -26,7 +29,9 @@ def test_workflow():
     w.execute()
 
 
-@pytest.mark.skip(reason="PyPhenologyDataset does not return geometry and datetime columns yet")
+@pytest.mark.skip(
+    reason="PyPhenologyDataset does not return geometry and datetime columns yet"
+)
 def test_main():
     file = "tests/recipes/pyphenology.yaml"
     main(file)

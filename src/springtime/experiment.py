5043d72ca49e431e540d6eeb65229d417b0eccb9
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2023-05-04 11:55:33 +0200

    Materialize estimator

--- src/springtime/experiment.py
+++ src/springtime/experiment.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from importlib import import_module
 from typing import Any, Literal, Sequence
 from pydantic import BaseModel
 
@@ -26,6 +27,8 @@ class RegressionExperiment(BaseModel):
     https://pycaret.readthedocs.io/en/latest/api/regression.html#pycaret.regression.setup
     for available arguments. 
     """
+    init_kwargs: dict[str, dict[str, Any]] | None
+    """Init kwargs for models that are not buildin pycaret models."""
     create_model: RegressionCreateModel | None
     """See
     https://pycaret.readthedocs.io/en/latest/api/regression.html#pycaret.regression.create_model
@@ -42,6 +45,43 @@ class RegressionExperiment(BaseModel):
     for available plot names."""
 
 
-
 # TODO: try to use pydantic validatedfunction to dynamically construct pydantic
 # models based on pycaret function signatures
+
+
+def materialize_estimators(
+    names: Sequence[str] | None,
+    buildin_models: Sequence[str],
+    init_kwargs: dict[str, dict[str, Any]] | None,
+):
+    """Materialize pycaret models from model names.
+
+    Args:
+        model_names (list[str]): list of model names
+        buildin_models (list[str]): list of buildin pycaret models
+        init_kwargs (dict[str,dict[str,Any]]): init kwargs for models
+
+    Returns:
+        list[str]: list of model names that can be materialized
+    """
+    if names is None:
+        return None
+    return [
+        materialize_estimator(model_name, buildin_models, init_kwargs)
+        for model_name in names
+    ]
+
+
+def materialize_estimator(name, buildin_models, init_kwargs):
+    if name in buildin_models:
+        return name
+    else:
+        module_name, class_name = name.rsplit(".", 1)
+        module = import_module(module_name)
+        model_class = getattr(module, class_name)
+        if init_kwargs and name in init_kwargs:
+            kwargs = init_kwargs[name]
+            model = model_class(**kwargs)
+        else:
+            model = model_class()
+        return model
--- src/springtime/main.py
+++ src/springtime/main.py
@@ -14,7 +14,12 @@ from pydantic import BaseModel, Field, validator
 from pycaret import regression
 
 from springtime.datasets import Datasets
-from springtime.experiment import ClassificationExperiment, RegressionExperiment
+from springtime.experiment import (
+    ClassificationExperiment,
+    RegressionExperiment,
+    materialize_estimator,
+    materialize_estimators,
+)
 from springtime.utils import PointsFromOther, resample, transponse_df
 
 logger = logging.getLogger(__name__)
@@ -129,24 +134,36 @@ class Workflow(BaseModel):
 
         s.setup(df, **self.experiment.setup)
         if self.experiment.create_model:
-            model = s.create_model(**self.experiment.create_model)
-            estimator = self.experiment.create_model["estimator"]
-            self.save_model(s, model, name=estimator)
-            self.plots_model(s, model, estimator)
+            cm = self.experiment.create_model
+            raw_estimator = cm["estimator"]
+            cm["estimator"] = materialize_estimator(
+                name=raw_estimator,
+                buildin_models=s.models().index,
+                init_kwargs=self.experiment.init_kwargs,
+            )
+            model = s.create_model(**cm)
+            self.save_model(s, model, raw_estimator)
+            self.plots_model(s, model, raw_estimator)
 
             if self.experiment.create_model["cross_validation"]:
                 self.save_leaderboard(s)
 
         if self.experiment.compare_models:
+            cm = self.experiment.compare_models
+            cm["include"] = materialize_estimators(
+                names=cm.get("include"),
+                buildin_models=s.models().index,
+                init_kwargs=self.experiment.init_kwargs,
+            )
             if self.experiment.compare_models["n_select"]:
-                best_models = s.compare_models(**self.experiment.compare_models)
+                best_models = s.compare_models(**cm)
                 for i, model in enumerate(best_models):
                     name = f"best#{i}"
                     self.save_model(s, model, name=name)
                     self.plots_model(s, model, name)
             else:
-                best_model = s.compare_models(**self.experiment.compare_models)
-                name="best"
+                best_model = s.compare_models(**cm)
+                name = "best"
                 self.save_model(s, best_model, name)
                 self.plots_model(s, best_model, name)
 
@@ -156,15 +173,15 @@ class Workflow(BaseModel):
     def plots_model(self, experiment, model, model_name):
         if self.experiment.plots is None:
             return
-        
+
         for plot_name in self.experiment.plots:
             self.plot_model(experiment, model, model_name, plot_name)
 
     def plot_model(self, experiment, model, model_name, plot_name):
         plot_fn_in_cwd = Path(experiment.plot_model(model, plot=plot_name, save=True))
-        plot_fn = self.session.output_dir / f'{model_name} {plot_fn_in_cwd.name}'
+        plot_fn = self.session.output_dir / f"{model_name} {plot_fn_in_cwd.name}"
         plot_fn_in_cwd.rename(plot_fn)
-        logger.warning(f'Saving {plot_name} plot to {plot_fn}')
+        logger.warning(f"Saving {plot_name} plot to {plot_fn}")
 
     def save_model(self, s, model, name):
         model_fn = self.session.output_dir / name

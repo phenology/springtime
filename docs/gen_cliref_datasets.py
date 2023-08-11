8ba41acf44f7ec8ccc517a56b278e77381e24219
Peter Kalverla <peter.kalverla@gmx.com>
2023-08-11 12:48:39 +0200

    Restructure data recipes and move to src

--- docs/gen_cliref_datasets.py
+++ docs/gen_cliref_datasets.py
@@ -4,15 +4,14 @@
 """Generate the code reference pages and navigation."""
 
 from pathlib import Path
-from textwrap import dedent
 
 import mkdocs_gen_files
 
 nav = mkdocs_gen_files.Nav()
 
-for path in sorted(Path("tests/recipes").rglob("*.yaml")):
-    module_path = path.relative_to("tests/recipes").with_suffix("")
-    doc_path = path.relative_to("tests/recipes").with_suffix(".md")
+for path in sorted(Path("src/springtime/recipes/datasets").rglob("*.yaml")):
+    module_path = path.relative_to("src/springtime/recipes/datasets").with_suffix("")
+    doc_path = path.relative_to("src/springtime/recipes/datasets").with_suffix(".md")
     full_doc_path = Path("clireference", doc_path)
 
     parts = tuple(module_path.parts)
@@ -22,24 +21,11 @@ for path in sorted(Path("tests/recipes").rglob("*.yaml")):
         recipe = input_file.readlines()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        fd.write(f'??? example "{parts[-1]}"\n')
-        fd.write("    ```yaml\n")
-        for line in recipe:
-                fd.write(f"    {line}")
-        fd.write("\n    ```\n")
+        fd.write("```yaml\n")
+        fd.writelines(recipe)
+        fd.write("\n```\n")
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)
 
 with mkdocs_gen_files.open("clireference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
-
-
-
-# from textwrap import dedent
-# with open('test.txt', 'w') as f:
-#     f.write(dedent(f"""
-#             ??? Example: {parts[-1]}
-#                 ```yaml
-#                 {{% include "{path}" %}}
-#                 ```
-#             """))
